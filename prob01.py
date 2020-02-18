2a57ef50f76c627c021641e5381213fb00baca04
Anthony <54080921+Ants0@users.noreply.github.com>
2020-02-18 12:22:01 -0800

    Update prob01.py

--- prob01.py
+++ prob01.py
@@ -1,4 +1,4 @@
 import math
-t = float(input("Enter the number of days after the colonization of the hive: "))
+t = float(input())
 p = round(100 * math.sqrt(t) + 201/(t+1)+1)
 print(str(int(t)) + " " + str(p))
