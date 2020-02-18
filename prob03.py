bfc52dd71488dbf8eb59f18e7f359a1a79c67cb4
Anthony <54080921+Ants0@users.noreply.github.com>
2020-02-18 12:21:45 -0800

    Update prob03.py

--- prob03.py
+++ prob03.py
@@ -1,22 +1,23 @@
-num = int(input("Enter the number of time values: "))
-final = 0
+num = int(input())
+
 for i in range(num):
-  timeValue = input("Enter a time value: ")
-  length = len(timeValue)
-  final = 20
-  for char in timeValue:
-      if(char == ":"):
-          continue
-      if(char == "1"):
-        final += 2 * 15
-      if(char == "7"):
-        final += 3 * 15
-      if(char == "4"):
-        final += 4 * 15
-      if(char == "2" or char == "3" or char == "5"):
-        final += 5 * 15
-      if(char == "6" or char == "9" or char == "0"):
-        final += 6 * 15
-      if(char == "8"):
-        final += 7 * 15
-  print(final)
+    final = 0
+    timeValue = input()
+    length = len(timeValue)
+    final = 20
+    for char in timeValue:
+        if(char == ":"):
+            continue
+        if(char == "1"):
+          final += 2 * 15
+        if(char == "7"):
+          final += 3 * 15
+        if(char == "4"):
+          final += 4 * 15
+        if(char == "2" or char == "3" or char == "5"):
+          final += 5 * 15
+        if(char == "6" or char == "9" or char == "0"):
+          final += 6 * 15
+        if(char == "8"):
+          final += 7 * 15
+    print(final, "milliamps")
