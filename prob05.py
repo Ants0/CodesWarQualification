9dac4202238094e321024441cdab394b8f751cd7
Anthony <54080921+Ants0@users.noreply.github.com>
2020-02-18 22:59:41 -0800

    Update prob05.py

--- prob05.py
+++ prob05.py
@@ -1,1 +1,31 @@
-
+sentence = input()
+list1 = []
+alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+pangram = -1
+counter = 0
+boolean = True
+for letter in sentence:
+  if(letter == " "):
+    continue
+  else:
+    list1 += letter
+list2 = list1[1:len(list1)]
+for char in alpha:
+  if(char in list1):
+    counter += 1
+if(counter == 26):
+  pangram = 0
+  for char in list1:
+    for letter in list2:
+      if(char == letter):
+        print(char, letter)
+        boolean = False
+      list2 = list2[1:]
+if(boolean == True and counter == 26):
+  pangram = 1
+if(pangram == -1):
+  print("Neither: " + sentence)
+if(pangram == 0):
+  print("Pangram: " + sentence)
+if(pangram == 1):
+  print("Perfect: " + sentence)
